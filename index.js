d584fda462ebb7fd1b16c2e4cbb2cd33818b215d
Jando <alexandroguterez@gmail.com>
2023-08-21 01:36:42 -0700

    add back cors options

--- index.js
+++ index.js
@@ -52,7 +52,10 @@ io.on('connection', socket => {
   });
 })
 
-app.use(cors());
+app.use(cors( {
+  origin: 'http://localhost:3000',
+  credentials: true,
+}));
 
 app.use(cookieParser());
 app.use(bodyParser.json());
